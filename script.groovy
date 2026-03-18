d79c3ff4e88bc8e8ca88bcfe7917e67942886a96
Nyati1 <69649175+Nyati1@users.noreply.github.com>
2026-03-18 13:58:24 -0600

    Refactor buildImage to use Jenkins credentials
    Updated buildImage function to securely handle Docker credentials and restructured build and push commands.

--- script.groovy
+++ script.groovy
@@ -6,14 +6,25 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     
-    // 1. Build using the subfolder context
+    /*// 1. Build using the subfolder context
     sh 'docker build -t njogud/demo-app:jma2.0 -f java-maven-app/Dockerfile java-maven-app/'
     
     // 2. Hardcoded login
     sh 'docker login -u njogud -p David@123'
     
     // 3. Push the image
-    sh 'docker push njogud/demo-app:jma2.0'
+    sh 'docker push njogud/demo-app:jma2.0'*/
+                   // 1. Securely pull the credentials from Jenkins
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        
+        // 2. Build the image using the subfolder context
+        sh 'docker build -t njogud/demo-app:jma2.0 -f java-maven-app/Dockerfile java-maven-app/'
+        
+        // 3. Login using double quotes and backslashes to inject the real password
+        sh "echo \$PASS | docker login -u \$USER --password-stdin"
+        
+        // 4. Push the image
+        sh 'docker push njogud/demo-app:jma2.0'
 } 
 
 def deployApp() {
