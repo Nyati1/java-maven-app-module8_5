1549ad7c629bfa629691967608ebeabda2119e1c
Nyati1 <69649175+Nyati1@users.noreply.github.com>
2026-03-18 13:51:41 -0600

    Clean up script by removing unused functions
    Removed commented-out functions and simplified the script.

--- script.groovy
+++ script.groovy
@@ -1,19 +1,3 @@
-/*def buildApp() {
-    echo 'building the application...'
-  
-}
-
-def testApp() {
-    echo 'testing the application...'
-   
-}
-
-def deployApp() {
-    echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
-  
-}*/
-
 def buildJar() {
     echo 'building the application...'
     sh 'mvn -f java-maven-app/pom.xml clean package'
@@ -30,25 +14,10 @@ def buildImage() {
     
     // 3. Push the image
     sh 'docker push njogud/demo-app:jma2.0'
-} // <--- THIS is the only bracket you need to close the function
-
-/* The commented out section is fine as it is */
+} 
 
 def deployApp() {
     echo 'deploying the application...'
 }
 
 return this
-
-
-/*def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t njogud/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push njogud/demo-app:jma-2.0'
-    }*/
-
-
-
-
