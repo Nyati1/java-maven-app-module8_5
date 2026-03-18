f59e516ac51fa90d4c68edf68ee28a439be5829e
Nyati1 <69649175+Nyati1@users.noreply.github.com>
2026-03-18 13:38:47 -0600

    Refactor buildImage function for Docker operations
    Refactored buildImage function to specify Dockerfile path and use updated image tag.

--- script.groovy
+++ script.groovy
@@ -23,10 +23,26 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        
+        // 1. Point to the subfolder where the Dockerfile and target/ folder actually live
+        // The '.' at the end tells Docker to use 'java-maven-app' as the context
+        sh 'docker build -t njogud/demo-app:jma2.0 -f java-maven-app/Dockerfile java-maven-app/'
+        
+        // 2. Use double quotes and backslashes to pass the credentials to the shell correctly
+        sh "echo \$PASS | docker login -u \$USER --password-stdin"
+        
+        // 3. Push the image
+        sh 'docker push njogud/demo-app:jma2.0'
+    }
+}
+
+/*def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t njogud/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push njogud/demo-app:jma-2.0'
-    }
+    }*/
 }
 
 def deployApp() {
