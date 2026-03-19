a3f309fe65c817d02eb42ed7a31a54b948754f83
Nyati1 <69649175+Nyati1@users.noreply.github.com>
2026-03-18 22:14:38 -0600

    Update script.groovy with Nexus URL

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildJar() {
     sh 'mvn clean package'
 }
 
-def buildImage() {
+/*def buildImage() {
     echo "building the docker image..."
     
     // Securely pull credentials from Jenkins Global Credentials
@@ -12,6 +12,25 @@ def buildImage() {
         sh 'docker build -t njogud/demo-app:jma2.0 .'
         sh "echo \$PASS | docker login -u \$USER --password-stdin"
         sh 'docker push njogud/demo-app:jma2.0'
+    }
+}*/
+def buildImage() {
+    echo "building and pushing to Nexus..."
+    
+    // 1. Define your Nexus URL
+    def nexusUrl = "159.203.37.26:8083" // Or your Nexus domain name
+    def imageName = "${nexusUrl}/demo-app:jma2.0"
+
+    withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        
+        // 2. Build the image with the Nexus URL in the name
+        sh "docker build -t ${imageName} ."
+        
+        // 3. Login to your PRIVATE Nexus server
+        sh "echo \$PASS | docker login -u \$USER --password-stdin ${nexusUrl}"
+        
+        // 4. Push to Nexus
+        sh "docker push ${imageName}"
     }
 }
 
